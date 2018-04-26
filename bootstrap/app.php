ea42ced6dd9f48ea91a2457eab544bf966821e48
tomual <tomual@users.noreply.github.com>
2018-04-26 17:57:30 -0500

    CSRF setup

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -46,19 +46,23 @@ $container['view'] = function ($container) {
     return $view;
 };
 
-$container['validator'] = function ($container) {
-    return new App\Validation\Validator;
-};
-
 $container['HomeController'] = function ($container) {
     return new \App\Controllers\HomeController($container);
 };
 $container['AuthController'] = function ($container) {
     return new \App\Controllers\Auth\AuthController($container);
 };
+$container['validator'] = function ($container) {
+    return new App\Validation\Validator;
+};
+$container['csrf'] = function ($container) {
+    return new \Slim\Csrf\Guard;
+};
 
 $app->add(new \App\Middleware\ValidationErrorsMiddleware($container));
 $app->add(new \App\Middleware\OldInputMiddleware($container));
+$app->add(new \App\Middleware\CsrfViewMiddleware($container));
+$app->add($container->csrf);
 
 v::with('App\\Validation\\Rules\\');
 
